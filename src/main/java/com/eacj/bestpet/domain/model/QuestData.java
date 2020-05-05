c413ab25373b6dd20e4f8c4978aedd831b4696e9
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-05-05 16:34:46 -0300

    update attribute name

--- src/main/java/com/eacj/bestpet/domain/model/QuestData.java
+++ src/main/java/com/eacj/bestpet/domain/model/QuestData.java
@@ -4,12 +4,76 @@ package com.eacj.bestpet.domain.model;
 public class QuestData {
     
     private Long id;
-    private int rA;
-    private int rB;
-    private int rC;
-    private int rD;
-    private int rE;
-    private int rF;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
     private int animal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+
+    public int getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(int animal) {
+        this.animal = animal;
+    }
     
 }
